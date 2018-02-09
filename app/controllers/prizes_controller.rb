db5d8f985986ca2f8a7ea0156c06a42477ed80b8
Hanna Bennett <bennetthanna22@gmail.com>
2018-02-08 22:54:59 -0800

    Add delete functionality

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -35,6 +35,13 @@ class PrizesController < ApplicationController
     end
   end
 
+  def destroy
+    @prize = Prize.find(params[:id])
+    @prize.destroy
+
+    redirect_to prizes_path
+  end
+
   private
 
   def prize_params
