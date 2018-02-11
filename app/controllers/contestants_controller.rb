e09e4c10b247aaf16320c0f3bd555bb7b5116fb5
Hanna Bennett <bennetthanna22@gmail.com>
2018-02-11 12:32:59 -0800

    Complete the creation of a contestant and redirects to show

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -1,10 +1,27 @@
 class ContestantsController < ApplicationController
+  def show
+    @contestant = Contestant.find(params[:id])
+  end
+
   def new
     @contestant = Contestant.new
     @prizes = Prize.all
   end
 
   def create
-    render plain: params[:prize_ids].inspect
+    # render plain: params[:prize_ids].inspect
+    # prize_ids = params[:prize_ids]
+    # contestant_name = params[:contestant]
+    @contestant = Contestant.new(contestant_params)
+    @contestant.prize_ids = params[:prize_ids]
+
+    @contestant.save
+    redirect_to @contestant
+  end
+
+  private
+
+  def contestant_params
+    params.require(:contestant).permit(:name)
   end
 end
