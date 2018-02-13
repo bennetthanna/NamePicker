d14dd8a3b5335e7d77ff76cdedd6cb70cfeb3b9c
Hanna Bennett <bennetthanna22@gmail.com>
2018-02-12 18:14:27 -0800

    Add validation for unique names and maximum of 3 prizes for contestants

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -13,9 +13,6 @@ class ContestantsController < ApplicationController
   end
 
   def create
-    # render plain: params[:prize_ids].inspect
-    # prize_ids = params[:prize_ids]
-    # contestant_name = params[:contestant]
     @contestant = Contestant.new(contestant_params)
     @contestant.prize_ids = params[:prize_ids]
 
--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,4 +1,6 @@
 class Contestant < ApplicationRecord
   serialize :prize_ids
-  validates :name, presence: true, length: { minimum: 2 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2 }
+  validates :prize_ids, length:
+    { maximum: 3, message: 'can only choose 3 prizes' }
 end
